52edd26144aca969a19e4f2ddcd7cf2f1dfad8c9
huynhthuan <huynhthuanvugia@gmail.com>
2022-03-03 23:48:09 +0700

    commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.features-slide', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 7,
+    spaceBetween: 24,
+    centeredSlides: true,
+    centeredSlidesBounds: true,
+    // If we need pagination
+    pagination: {
+        el: '.features-pagination',
+    },
+});
+
+const swiper2 = new Swiper('.slide-team-inner', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 40,
+
+    navigation: {
+        nextEl: '.team-button-next',
+        prevEl: '.team-button-prev',
+    },
+});
