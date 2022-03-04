e878e0c4259df3007ece3b94b8b5286e9805a957
huynhthuan <huynhthuanvugia@gmail.com>
2022-03-04 16:30:22 +0700

    add responsive

--- js/script.js
+++ js/script.js
@@ -10,6 +10,18 @@ const swiper = new Swiper('.features-slide', {
         clickable: true,
         el: '.features-pagination',
     },
+    breakpoints: {
+        1024: {
+            slidesPerView: 5,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+    },
 });
 
 const swiper2 = new Swiper('.slide-team-inner', {
@@ -22,8 +34,37 @@ const swiper2 = new Swiper('.slide-team-inner', {
         nextEl: '.team-button-next',
         prevEl: '.team-button-prev',
     },
+    breakpoints: {
+        1368: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        0: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+    },
 });
 
 AOS.init({
     duration: 1000,
+});
+
+$('.btn-toggle-menu').click(() => {
+    $('.main-menu-wrap').toggleClass('active');
+});
+
+$('.btn-close-menu').click(() => {
+    $('.main-menu-wrap').removeClass('active');
 });
