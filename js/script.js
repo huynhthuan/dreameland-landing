57b5e60a95ade999b7bbc81a85d4af5bbfe66cfe
huynhthuan <huynhthuanvugia@gmail.com>
2022-03-05 21:53:23 +0700

    Add responsive

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 const swiper = new Swiper('.features-slide', {
     // Optional parameters
     loop: true,
-    slidesPerView: 7,
+    slidesPerView: 3,
     spaceBetween: 24,
-    centeredSlides: true,
-    centeredSlidesBounds: true,
     // If we need pagination
     pagination: {
         clickable: true,
         el: '.features-pagination',
     },
+    navigation: {
+        nextEl: '.features-button-next',
+        prevEl: '.features-button-prev',
+    },
     breakpoints: {
-        1024: {
-            slidesPerView: 5,
+        1200: {
+            slidesPerView: 3,
         },
         768: {
             slidesPerView: 3,
@@ -20,6 +22,39 @@ const swiper = new Swiper('.features-slide', {
         0: {
             slidesPerView: 1,
             spaceBetween: 0,
+        },
+    },
+    on: {
+        slideChange: function (swiper) {
+            $('.features-slide .swiper-slide').each((index, item) => {
+                if (window.innerWidth <= 1024) {
+                    $(item).css('right', '0');
+                } else if (window.innerWidth <= 1200) {
+                    switch (swiper.realIndex + 1) {
+                        case 1:
+                            $(item).css('right', '-31%');
+                            break;
+                        case 2:
+                            $(item).css('right', '-11%');
+                            break;
+                        case 3:
+                            $(item).css('right', '-21%');
+                            break;
+                    }
+                } else {
+                    switch (swiper.realIndex + 1) {
+                        case 1:
+                            $(item).css('right', '-30%');
+                            break;
+                        case 2:
+                            $(item).css('right', '-10%');
+                            break;
+                        case 3:
+                            $(item).css('right', '-20%');
+                            break;
+                    }
+                }
+            });
         },
     },
 });
@@ -35,6 +70,10 @@ const swiper2 = new Swiper('.slide-team-inner', {
         prevEl: '.team-button-prev',
     },
     breakpoints: {
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
         1368: {
             slidesPerView: 3,
             spaceBetween: 40,
@@ -44,7 +83,7 @@ const swiper2 = new Swiper('.slide-team-inner', {
             spaceBetween: 40,
         },
         1024: {
-            slidesPerView: 3,
+            slidesPerView: 4,
             spaceBetween: 40,
         },
         768: {
@@ -67,4 +106,16 @@ $('.btn-toggle-menu').click(() => {
 
 $('.btn-close-menu').click(() => {
     $('.main-menu-wrap').removeClass('active');
+});
+
+$('.player-btn').click(() => {
+    console.log('btn');
+    let video = $('.gameplay-player video')[0];
+    if (video.paused) {
+        video.play();
+        $('.player-btn').hide();
+    } else {
+        video.pause();
+        $('.player-btn').show();
+    }
 });
